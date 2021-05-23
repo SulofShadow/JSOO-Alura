aaa95cd706f63549a34f6bdbfe24eea0e0d12118
João Victor Guimarães <81489921+SulofShadow@users.noreply.github.com>
2021-05-23 02:31:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const cliente2 = new Cliente("Maria", 15773370783);
 const contaCorrenteJoao = new ContaCorrente(1001, cliente1);
 const conta2 = new ContaCorrente(120, cliente2);
 
-// Como ele não pertence à alguma intancina, nós chamamos a própria ContaCorrete(classe);
+// Como ele não pertence à alguma instancia, nós chamamos a própria ContaCorrete(classe);
 console.log(ContaCorrente.numeroDeContas);
